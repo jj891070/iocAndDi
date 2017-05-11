50174ef49711b5cb859e9302f634b1db12fdcb76
jay_liao <chungyojay0206@gmail.com>
2017-05-11 12:09:23 +0800

    update

--- main.php
+++ main.php
@@ -1,19 +1,23 @@
 <?php
+//載入檔案
 if (!defined('DIR')) define("DIR",dirname(__FILE__));
-
 require_once DIR.'/Service/catch_Service.php';
 require_once DIR.'/Service/analysis_Service.php';
 require_once DIR.'/honkongAnalysis.php';
 require_once DIR.'/honkongGetRaw.php';
 
-$url='http://bet.hkjc.com/football/index.aspx?lang=ch';		
+//設定要抓的url
+$url='http://bet.hkjc.com/football/index.aspx?lang=ch';	
+
+//抓取網頁原始碼，儲存在$original
 $catch = new catch_Service( new honkongGetRaw());
 $original = $catch->getRaw($url);
+
+//分析原始碼，取出賽事資料，儲存在$event
 $analysis = new analysis_Service( new honkongAnalysis());
 $event = $analysis->allGameInformation($original);
 
 $event=json_encode($event,JSON_UNESCAPED_UNICODE);
 print_r($event);
-// var_dump($event);
 die();
 ?>
